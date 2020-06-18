71e667575ec1beaae9839b207d972ce0ea42c2c8
ansary14 <ayubuapcse@gmail.com>
2020-06-18 09:38:02 +0600

    Updated webpack for checking javascript

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,10 +2,11 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require("webpack")
 
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
+	environment.plugins.append("Provide", new webpack.ProvidePlugin({
 	$: 'jquery',
 	jQuery: 'jquery',
 	Popper: ['popper.js', 'default']
+	
 }))
 
 module.exports = environment
