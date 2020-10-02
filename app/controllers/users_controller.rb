24d64b25844ef00c3579ef1f61ed78abe3f11ee9
ansary14 <ayubuapcse@gmail.com>
2020-10-02 09:05:34 +0600

    Complete category index and show views and edit categories

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
-    @articles = @user.articles.paginate(page: params[:page], per_page: 3)
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 3)
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
